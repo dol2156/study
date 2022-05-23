703ffa4e393037829cbe23e834b752d776799f22
dol2156 <dol2156@gmail.com>
2022-05-23 18:58:41 +0900

    state 초입

--- tutorials/react/react-app/src/App.js
+++ tutorials/react/react-app/src/App.js
@@ -5,15 +5,29 @@ import Toc from "./components/Toc";
 import Subject from "./components/Subject";
 import Content from "./components/Content";
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      subject : {
+        title : 'WEB',
+        sub : 'World Wid Web!!!',
+      },
+      content : {
+        title : 'HTML',
+        desc : 'HTML is Hyper Text Markup Language.',
+      }
+      
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <Toc></Toc>
-        <Content title="HTML" desc="HTML is Hyper Text Markup Language."></Content>
+        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
       </div>
     )
   }
