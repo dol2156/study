8ab2b9196f08ecf975283aef5a7976cd9974df3c
dol2156 <dol2156@gmail.com>
2022-05-23 18:09:20 +0900

    컴포넌트 화

--- tutorials/react/react-app/src/App.js
+++ tutorials/react/react-app/src/App.js
@@ -1,25 +1,61 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component {
+  render() {
+    return (
+      <header className="Subject">
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
+    )
+  }
+}
+
+class Toc extends Component {
+  render() {
+    return (
+      <ul className="Toc">
+        <li>
+          <a href="1.html">HTML</a>
+        </li>
+        <li>
+          <a href="2.html">CSS</a>
+        </li>
+        <li>
+          <a href="3.html">Javascript</a>
+        </li>
+      </ul>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article className="Content">
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Toc></Toc>
+        <Content></Content>
+        Hello, React!!!
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+
