3d4020a558f15bf274e1de7d271341d98ca77e36
강석민 <dol2156@gmail.com>
2022-05-25 22:21:13 +0900

    react ajax init

---
+++ tutorials/react/react-ajax/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li><a href='/'>HTML</a></li>
+          <li><a href='/'>CSS</a></li>
+          <li><a href='/'>JS</a></li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React & Ajax
+      </article>
+    </div>
+  );
+}
+
+export default App;
