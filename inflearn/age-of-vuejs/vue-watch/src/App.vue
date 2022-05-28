e37e6caa025803b229d1ace3cb2e0fed222278eb
dol2156 <dol2156@gmail.com>
2022-05-28 17:03:10 +0900

    초급 강좌 종료

--- inflearn/age-of-vuejs/vue-watch/src/App.vue
+++ inflearn/age-of-vuejs/vue-watch/src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>App</div>
+  <div>{{ message }}</div>
+  <div>{{ num }}</div>
+  <div>{{ property }}</div>
+  <div>
+    <button type="button" @click="addNum">increase</button>
+  </div>
+  <div :class="cname">Hello</div>
+  <div :class="errorTextColor">Hello</div>
+  <div>
+    <button type="button" @click="changeWarning">WARNING</button>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name : "App",
+  props : ['propsdata'],
+  data() {
+    return {
+      message : "Hello Vue",
+      num : 10,
+      cname : 'blue-text',
+      isError : false,
+    };
+  },
+  methods : {
+    addNum() {
+      this.num = this.num + 1;
+    },
+    changeWarning(){
+      this.isError = true;
+    }
+  },
+  watch : {
+    // num 이 변경될 때마다, 이 함수가 실행될 것 입니다.
+    num(new_num, old_num) {
+      console.log(new_num, old_num);
+    }
+  },
+  computed : {
+    property() {
+      return this.num * 2;
+    },
+    errorTextColor() {
+      if(this.isError===true){
+        return "warning";
+      }else{
+        return null;
+      }
+    },
+  },
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+.warning{ color:red; }
 </style>
