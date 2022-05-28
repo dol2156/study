ae29f1d3af655e3f4c5ca4007b93f42ac0e38f83
dol2156 <dol2156@gmail.com>
2022-05-28 13:33:03 +0900

    Update App.vue

--- inflearn/age-of-vuejs/vue-template-syntax/src/App.vue
+++ inflearn/age-of-vuejs/vue-template-syntax/src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>App</div>
+  <div>{{ message }}</div>
+  <div>{{ doubleNum }}</div>
+  <div :id="uuid">{{ uuid }}</div>
+  <div :class="class_name">{{ class_name }}</div>
+  <div v-if="loading===true">
+    Loading...
+  </div>
+  <div v-else>
+    로그인 되었습니다.
+  </div>
+  <div v-show="loading===true">
+    Loading...
+  </div>
+  <button type="button" @click="onLogin">LOGIN</button>
+  <div>
+    <input
+      type="text" name="message" placeholder="placeholder"
+      v-model="message"
+    />
+  </div>
+  <div>
+    <input
+      type="text" name="keyup_test" placeholder="placeholder"
+      @keyup.enter="onLogTxt"
+    />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name : "App",
+  data() {
+    return {
+      message : "Hello Vue",
+      num : 10,
+      uuid : 'abc123',
+      class_name : 'text_blue',
+      loading : true,
+    };
+  },
+  computed : {
+    doubleNum() {
+      return this.num * 2;
+    }
+  },
+  methods : {
+    onLogin() {
+      this.loading = false;
+    },
+    onLogTxt() {
+      console.log("onLogTxt");
+    },
+  },
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+.text_blue{ color:blue; }
 </style>
