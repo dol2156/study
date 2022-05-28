bf2b7cc3da492e392aa40e542f089cf90a67545d
dol2156 <dol2156@gmail.com>
2022-05-28 18:22:50 +0900

    1

---
+++ inflearn/age-of-vuejs/vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <TodoHeader/>
+  <TodoInput/>
+  <TodoList/>
+  <TodoFooter/>
+</template>
+<script>
+import TodoHeader from "@/components/TodoHeader";
+import TodoInput from "@/components/TodoInput";
+import TodoList from "@/components/TodoList";
+import TodoFooter from "@/components/TodoFooter";
+export default {
+  name : "App",
+  components : {TodoFooter, TodoList, TodoInput, TodoHeader},
+  data() {
+    return {
+      message : "Hello Vue",
+      num : 10,
+    };
+  },
+  computed : {
+    doubleNum() {
+      return this.num * 2;
+    }
+  },
+}
+</script>
+<style>
+</style>
---
+++ inflearn/age-of-vuejs/vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>TodoFooter : {{ message }}</div>
+</template>
+<script>
+export default {
+  name : "TodoFooter",
+  props : ['propsdata'],
+  data() {
+    return {
+      message : "Hello Vue",
+      num : 10,
+    };
+  },
+  computed : {
+    doubleNum() {
+      return this.num * 2;
+    }
+  },
+  watch : {
+    // num 이 변경될 때마다, 이 함수가 실행될 것 입니다.
+    num(new_num, old_num) {
+      console.log(new_num, old_num);
+    }
+  },
+  methods : {
+    onFunc() {
+      console.log("onFunc");
+    },
+  },
+}
+</script>
+<!--CSS를 이 구성 요소로만 제한하려면 "범위 지정" 속성을 추가하십시오.-->
+<style scoped>
+</style>
