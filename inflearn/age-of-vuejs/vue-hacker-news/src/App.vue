ccbfef17bda473d7faccfc25008d609ab2f0d751
dol2156 <dol2156@gmail.com>
2022-06-02 18:48:09 +0900

    ㅇㄹㅇㄹ

---
+++ inflearn/age-of-vuejs/vue-hacker-news/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>App : {{ message }}</div>
+</template>
+<script>
+export default {
+  name : "App",
+  data() {
+    return {
+      message : "Hello Vue",
+    };
+  },
+}
+</script>
+<style>
+</style>
