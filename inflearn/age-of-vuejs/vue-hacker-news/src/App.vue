5781af52208f7b7c4941150ed90cc462a62fbc51
dol2156 <dol2156@gmail.com>
2022-06-02 21:57:48 +0900

    ㅇㅇㅇ

--- inflearn/age-of-vuejs/vue-hacker-news/src/App.vue
+++ inflearn/age-of-vuejs/vue-hacker-news/src/App.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>App : {{ message }}</div>
+  <header>
+    <nav>
+      <ul>
+        <li class="on">
+          <a href="/">News</a>
+        </li>
+        <li>
+          <a href="/">Ask</a>
+        </li>
+        <li>
+          <a href="/">Jobs</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main></main>
 </template>
 <script>
 export default {
@@ -12,4 +27,14 @@ export default {
 }
 </script>
 <style>
+header{ outline:1px dashed red; box-shadow:0px 0px 10px cornflowerblue; }
+header{ background-color:green; }
+header > nav{ padding-left:10px; padding-right:10px; }
+header > nav > ul{}
+header > nav > ul > li{ display:inline-block; }
+header > nav > ul > li + li{ margin-left:10px; }
+header > nav > ul > li > a{ color:white; font-size:20px; line-height:35px; }
+header > nav > ul > li > a{ opacity:0.5; }
+header > nav > ul > li.on > a{ opacity:1; }
+header > nav > ul > li > a:hover{ text-decoration:underline; }
 </style>
