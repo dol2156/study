ab865b0ef93cc88b7f3b13ebf70d9cc5819d8c6d
dol2156 <dol2156@gmail.com>
2022-06-02 22:15:40 +0900

    ㅇㅇ

--- inflearn/age-of-vuejs/vue-hacker-news/src/main.js
+++ inflearn/age-of-vuejs/vue-hacker-news/src/main.js
@@ -1,8 +1,12 @@
 import '@/assets/css/reset.css';
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import App from './App.vue';
-import router_index from '@/routes/index'
+import router_index from '@/routes/index';
+import {createPinia} from "pinia";
+import {useCommonStore} from "@/store/common";
 
 const app = createApp(App);
 app.use(router_index);
+app.use(createPinia());
+app.config.globalProperties.$commonStore = useCommonStore(); // 전역 설정
 app.mount('#app');
