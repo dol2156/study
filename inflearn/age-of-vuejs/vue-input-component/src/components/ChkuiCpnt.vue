5e54dac3fa20c36d31f9484d76bfff8bc8c8db7e
강석민 <dol2156@gmail.com>
2022-06-08 19:33:22 +0900

    커스텀 체크 박스 만들기

---
+++ inflearn/age-of-vuejs/vue-input-component/src/components/ChkuiCpnt.vue
@@ -0,0 +1,46 @@
+<template>
+  <label class="chkui">
+    <input type="checkbox" :checked="isChecked" @change="onChange(chkIdx)" />
+    <div>{{ chkIdx }} {{ isChecked }}</div>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    chkIdx: {
+      type: Number,
+    },
+    isChecked: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: {
+    onChange(index) {
+      const checked = event.target.checked;
+      this.is_checked = checked;
+      this.$emit("eChange", index, checked);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chkui {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  outline: 1px solid red;
+  width: 200px;
+  height: 35px;
+  user-select: none;
+}
+.chkui:hover {
+  background-color: #eee;
+}
+.chkui > * {
+  cursor: pointer;
+}
+</style>
