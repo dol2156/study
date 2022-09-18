ebbcf44b364c05c6ad0fdc2c9bdb76655898f32f
강석민 <dol2156@gmail.com>
2022-09-18 20:58:32 +0900

    웹팩 기본 정리 끝

--- tutorials/webpack_practice/src/index.js
+++ tutorials/webpack_practice/src/index.js
@@ -2,6 +2,10 @@ import './css/reset.css';
 import './css/common.css';
 import common_js from "./js/common.js";
 import layout_js from "./js/layout.js";
+import _ from 'lodash';
 
 common_js.init();
 layout_js.init();
+
+const k = _.join(['😊', '💀', '👍'], '/');
+console.log(k);
