05d905be26de2d6e88c409f52ab6d200f386bf5d
강석민 <dol2156@gmail.com>
2024-02-26 08:51:53 +0900

    ddd

---
+++ demo/gsap/02_advenced/02_DeepDive/2_4_utils_wrap/main.js
@@ -0,0 +1,15 @@
+GSDevTools.create();
+
+$(function () {
+  const $stage = $(`.stage`);
+  gsap.to($stage, { autoAlpha: 1 });
+
+  const $H1 = $(`H1`);
+
+  gsap.from($H1.find('.char'), {
+    //y: 'random(-100,100)',
+    y: gsap.utils.wrap([100, -100]),// 지그재그 순환하여 넣어줌
+    opacity: 0,
+    stagger: 0.02,
+  });
+});
