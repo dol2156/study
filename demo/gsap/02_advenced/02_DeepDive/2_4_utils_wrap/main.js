c489a41767d20f9e6d13508ce2889fdf97e82b8d
강석민 <dol2156@gmail.com>
2024-02-26 09:58:16 +0900

    utils_wrap

--- demo/gsap/02_advenced/02_DeepDive/2_4_utils_wrap/main.js
+++ demo/gsap/02_advenced/02_DeepDive/2_4_utils_wrap/main.js
@@ -2,9 +2,11 @@ $(function () {
   const $stage = $(`.stage`);
   gsap.to($stage, { autoAlpha: 1 });
 
+  const tlH1 = gsap.timeline({});
   const $H1 = $(`H1`);
+  const $chars = $H1.find('.char');
 
-  gsap.from($H1.find('.char'), {
+  tlH1.from($chars, {
     //y: 'random(-100,100)',
     y: gsap.utils.wrap([100, -100]), // 지그재그 순환하여 넣어줌
     opacity: 0,
@@ -12,5 +14,11 @@ $(function () {
       each: 0.02,
       from: 'random',
     },
+  });
+
+  tlH1.to($chars, {
+    x: 10,
+    color:gsap.utils.wrap(['red', 'blue']),
+    stagger: 0.02,
   });
 });
