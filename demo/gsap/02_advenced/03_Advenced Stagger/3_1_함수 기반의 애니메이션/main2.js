9f329ae9261f44884c7fb0b44fcb9d82b9e3d291
강석민 <dol2156@gmail.com>
2024-02-27 16:33:23 +0900

    Update main2.js

--- demo/gsap/02_advenced/03_Advenced Stagger/3_1_함수 기반의 애니메이션/main2.js
+++ demo/gsap/02_advenced/03_Advenced Stagger/3_1_함수 기반의 애니메이션/main2.js
@@ -4,9 +4,27 @@ $(function () {
 
 function startAnimate() {
   const $tigerList = $(`.wrap > div`);
-  console.log($tigerList);
 
   gsap.to($tigerList, {
-    rotation: 360,
+    duration: function (_i, _t) {
+      if ($(_t).hasClass('blue')) {
+        return 0.5;
+      } else {
+        return 1;
+      }
+    },
+    rotation: function (_i, _t) {
+      if ($(_t).hasClass('blue')) {
+        return -360;
+      } else {
+        return 360;
+      }
+    },
+    repeat: -1,
+    ease: 'none',
+    stagger: {
+      each: 0.2,
+      from: 'random',
+    },
   });
 }
