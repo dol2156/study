ef06384cb51662f1316c175e30fadff7c22c8457
강석민 <dol2156@gmail.com>
2024-02-27 16:47:06 +0900

    가속도 기반의 스태거

--- demo/gsap/02_advenced/03_Advenced Stagger/3_2_가속도 기반의 Stagger/main.js
+++ demo/gsap/02_advenced/03_Advenced Stagger/3_2_가속도 기반의 Stagger/main.js
@@ -1,12 +1,16 @@
+$(function () {
+  startAnimate();
+});
 
-
-
-
-
-
-
-
-
-
-
-
+function startAnimate() {
+  gsap.from(`.stage > div`, {
+    duration: 2,
+    opacity: 0,
+    scale: 0,
+    stagger: {
+      each: 0.2,
+      ease: `power4.inOut`,
+      from: `center`,
+    },
+  });
+}
